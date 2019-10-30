15b4d720cd629824cfb26822edd9ccb546fde229
ioanasteliana <57158261+ioanasteliana@users.noreply.github.com>
2019-10-30 10:59:31 +0200

    Create tema_de_casa3.m

---
+++ tema_de_casa3.m
@@ -0,0 +1,31 @@
+f=4;
+t=0:0.002:0.5
+s=square(2*pi*f*t)
+s(find(t>=0 & t<0.25))=1;
+s(find(t>=0.25 & t<0.5))=-1;
+figure(1)
+plot(t,s)
+xlabel('Timp [s]'),ylabel('Amplitudine'),grid
+
+%Pentru rezolutia temporara de 20ms
+
+%Subpunctul a)
+f=4;
+t=0:0.02:0.5
+s=square(2*pi*f*t)
+s(find(t>=0 & t<0.25))=1;
+s(find(t>=0.25 & t<0.5))=-1;
+figure(2)
+plot(t,s)
+xlabel('Timp [s]'),ylabel('Amplitudine'),grid
+
+%Pentru rezolutia temporara de 200ms
+
+f=4;
+t=0:0.2:0.5
+s=square(2*pi*f*t)
+s(find(t>=0 & t<0.25))=1;
+s(find(t>=0.25 & t<0.5))=-1;
+figure(3)
+plot(t,s)
+xlabel('Timp [s]'),ylabel('Amplitudine'),grid
